57c4fed0cbf6b389998b8c9dae629b99ca960989
Gary A. Stafford <garystafford@rochester.rr.com>
2018-09-14 16:04:20 -0400

    Refactor docker build script

--- Docker/docker-build-push.sh
+++ Docker/docker-build-push.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 ./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/wp-es-demo:latest .
+
+docker build -f Dockerfile --no-cache -t garystafford/wp-es-demo:latest .
+
+# Docker Hub
 docker push garystafford/wp-es-demo:latest
+
+# GCP Container Registry
+docker tag garystafford/wp-es-demo:latest gcr.io/wp-search-bot/wp-es-demo:latest
+docker push gcr.io/wp-search-bot/wp-es-demo:latest
+
+kubectl replace --force -f ../gke/wp-es-demo.yaml
 
 # docker run --name wp-es-demo -p 8080:8080 -d garystafford/wp-es-demo:latest
 # docker logs $(docker ps | grep wp-es-demo | awk '{print $NF}') --follow
